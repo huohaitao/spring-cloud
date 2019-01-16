19a2a2ff31eb0e24924f641dcf13d7129a8a750a
huohaitao <37256890+huohaitao@users.noreply.github.com>
2019-01-16 10:04:40 +0800

    @SpringCloudApplication

--- eureka-consumer-feign/src/main/java/com/haitao/consumer/FeignConsumerApplication.java
+++ eureka-consumer-feign/src/main/java/com/haitao/consumer/FeignConsumerApplication.java
@@ -4,23 +4,22 @@
 package com.haitao.consumer;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.ComponentScan;
 
 /**
+ * 
+ * @SpringCloudApplication = @SpringBootApplication + @EnableDiscoveryClient + @EnableCircuitBreaker
  * 使用最基础的方式调用eureka服务
  * @author huoht
  *
  */
+
 @EnableHystrixDashboard
-@EnableCircuitBreaker
-@EnableDiscoveryClient
 @EnableFeignClients
-@SpringBootApplication
+@SpringCloudApplication
 @ComponentScan({"com.haitao"})
 public class FeignConsumerApplication {
 
